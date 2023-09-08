d1c7064e17f7680781712b738c9a66908eb05a46
jantsandorj <102676794+kieunMoon@users.noreply.github.com>
2023-09-08 13:47:21 +0800

    import fonts by @font-face

--- assets/js/common.js
+++ assets/js/common.js
@@ -19,6 +19,19 @@ const swiper1 = new Swiper(".swiper1", {
   mousewheel: true,
   pagination: {
     el: ".swiper-pagination",
+  },
+});
+const aside_banner = new Swiper(".aside_banner", {
+  effect: "fade",
+  crossFade: true,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
     dynamicBullets: true,
   },
 });
