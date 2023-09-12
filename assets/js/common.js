336343f78b7916baeafc640cbb361cb652cb559b
jantsandorj <102676794+kieunMoon@users.noreply.github.com>
2023-09-12 11:34:54 +0800

    main responsive

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,10 +2,10 @@ const swiper = new Swiper(".swiper", {
   effect: "fade",
   crossFade: true,
   loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
   mousewheel: true,
   navigation: {
     nextEl: ".swiper-button-next",
@@ -16,10 +16,10 @@ const swiper1 = new Swiper(".swiper1", {
   effect: "fade",
   crossFade: true,
   loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
   mousewheel: true,
   pagination: {
     el: ".swiper-pagination",
@@ -29,10 +29,10 @@ const aside_banner = new Swiper(".aside_banner", {
   effect: "fade",
   crossFade: true,
   loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
   mousewheel: true,
   pagination: {
     el: ".swiper-pagination",
